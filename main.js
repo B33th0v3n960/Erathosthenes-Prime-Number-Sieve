d6b4595311301be9d0c64f6f5697dbdbd6e6b290
Zhou Sun <zhousun06@gmail.com>
2022-07-05 16:14:49 +0100

    optimise the code

--- main.js
+++ main.js
@@ -1,75 +1,22 @@
 let list = [];
 
-for (let i = 1; i <= 50000; i++) {
-  list.push(i);
-}
-
+showPrime(1000000);
 console.log(list);
-let primeNum = primeSieve(list);
+// list.forEach((num) => console.log(num));
+console.log(list.length);
 
-primeNum.forEach((num) => checkPrime(num));
-console.log(primeNum.length);
-
-console.log(primeNum);
-
-function primeSieve(array) {
-  let newArray = [];
-  let firstPrime = 2;
-
-  for (num of array) {
-    if (num === 1) continue;
-    if (num === 2) newArray.push(num);
-    if (num % firstPrime === 0) continue;
-    newArray.push(num);
+function showPrime(limit) {
+  for (let number = 2; number <= limit; number++) {
+    if (checkPrime(number)) list.push(number);
   }
-
-  let cache = newArray;
-  let tempMemory = cache;
-  newArray = [];
-
-  for (let i = 0; i < cache.length; i++) {
-    for (num of tempMemory) {
-      if (num === tempMemory[i]) {
-        newArray.push(num);
-        continue;
-      }
-
-      if (tempMemory[i] / 2 > tempMemory[tempMemory.length - 1])
-        return tempMemory;
-      if (num % tempMemory[i] === 0) continue;
-
-      newArray.push(num);
-    }
-    tempMemory = newArray;
-    newArray = [];
-  }
-  return tempMemory;
 }
 
 function checkPrime(num) {
-  const number = num;
-  let isPrime = true;
-
-  if (number === 1) {
-    console.log("1 is neither prime nor composite number.");
-  }
-
-  // check if number is greater than 1
-  else if (number > 1) {
-    // looping through 2 to number-1
-    for (let i = 2; i < number; i++) {
-      if (number % i == 0) {
-        isPrime = false;
-        break;
-      }
+  for (let factor = 2; factor < num; factor++) {
+    if (factor > num / 2) break;
+    if (num % factor === 0) {
+      return false;
     }
-
-    if (isPrime) {
-      console.log(`${number} is a prime number`);
-    } else {
-      console.log(`${number} is a not prime number`);
-    }
-  } else {
-    console.log("The number is not a natural number.");
   }
+  return true;
 }
